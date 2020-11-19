896f4682f8e850c0ada8cff806e85bda42fada22
Barkhayot Juraev <coderviewer@gmail.com>
2020-11-19 20:14:51 +0900

    constructor

---
+++ constructors_default.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Cube 
+{
+    public :
+    int side;
+    Cube()
+    {
+        side = 10;
+    }
+};
+
+int main()
+{
+    Cube c;
+    cout << c.side <<endl;
+
+    return 0;
+}
+
